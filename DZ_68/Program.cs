1a6a4c7dbfc03bfdb26aa8764fcbc6c50ab01562
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-11-27 22:29:31 +0300

    Add DZ_68

---
+++ DZ_68/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Программу вычисления функции Аккермана с помощью рекурсии");
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Задайте значение M = ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Задайте значение N = ",""); 
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+
+int FunctionAkkerman = akkerman(m, n);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write($"Функция Аккермана = {FunctionAkkerman} ");
+
+int akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return akkerman(m - 1, 1);
+  else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
